8696f90c55fcfaa2f85acc7c71df9be3c9d7c81b
Patrick <patrick.yrkim@gmail.com>
2020-05-26 23:08:40 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HybridApp from './src/App'
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    // </View>
-    <HybridApp />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// what the react-scripts will look for to render the app
+
+import React from 'react';
+import ReactDOM from 'react';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
